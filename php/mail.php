7db9cd5b45f00a4d015b81af53a7a80f56abeae8
Sami Hattab <hattab_sami@yahoo.fr>
2019-06-18 10:40:35 +0200

    contact debug

--- php/mail.php
+++ php/mail.php
@@ -17,7 +17,7 @@ if(empty($_POST['prenom']) ||
     $mail = strip_tags(htmlspecialchars($_POST['mail'])) ;
     $phone = strip_tags(htmlspecialchars($_POST['phone'])) ;
     $sujet = strip_tags(htmlspecialchars($_POST['sujet'])) ;
-    $message = strip_tags(htmlspecialchars(utf8_decode($_POST['message']))) ;
+    $message = htmlspecialchars(utf8_decode($_POST['message'])) ;
     
     // $message_final = $prenom." ".$nom." < " .$mail. " > \n\n";
     // $message_final .= $phone."\n\n\n";
