c320576cf2c615a6033f257daae1b559e5b16f7d
Sami Hattab <hattab_sami@yahoo.fr>
2019-06-18 10:09:13 +0200

    contact debug

--- php/mail.php
+++ php/mail.php
@@ -24,7 +24,7 @@ if(empty($_POST['prenom']) ||
     $message_final .= $message."\"";
     
      
-    $to = 'hattab_sami@yahoo.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+    $to = 'hattab_sami@yahoo.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
     $subject = "Website Contact Form:  $name";
     $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $prenom"." "." $nom\n\nEmail: $mail\n\nPhone: $phone\n\nMessage:\n$message";
     $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
